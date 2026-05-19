c130ac97b70d0ca965592ae045b419c805cd1621
Hamanaka-Yuki-TP <282408037+Hamanaka-Yuki-TP@users.noreply.github.com>
2026-05-19 10:47:44 +0900

    Change1

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "Loans.h"
 
 int main(void){
     int i=1;// ループ制御用の変数
@@ -21,7 +22,7 @@ int main(void){
     int choice;//選択肢格納場所
     int state;
 
-    while(i == 1){
+    while(1){
         printf("\n==================================\n");
         printf("\n           メインメニュー          \n");
         printf("\n==================================\n");
@@ -29,42 +30,13 @@ int main(void){
         scanf("%d",&state);//１，２，３を選択
 
         if(state == 1){//貸出
-            switch(state){
-            case 1://ID確認
-            printf("\nIDを入力して下さい。\n\n");
-            scanf("%d",&p_ID);
-            if(p_ID == 1){
-                state == 2;
-            }else if(p_ID == 0){
-                state == 3;
-                //printf("ID確認へ戻る\n");
-            }
-            break;
-            case 2://本コードを入力
-            printf("\n本コードを入力して下さい。\n\n");
-            scanf("%d",&b_ID);
-            if(b_ID == 0){
-                state <= state + 1;
-            }else{
-                state <= 2;
-            }
-            case 3://期間の設定
-            printf("貸出期間を設定して下さい。\n");
-            printf("４泊５日[1]または７泊８日[2]\n");
-            scanf("%d",&rentalday);
-            case 4://確認
-            printf("〇泊〇日で間違いないですか。\n");
-            case 5://支払い
-            default:
-            printf("3つの項目から選んで下さい。\n");
-            break;
-        }
+           Loans();
         }else if(state == 2){//検索
 
         }else if(state == 3){//ID登録
 
         }else{//１，２，３以外の時
-
+           printf("\n[1],[2],[3]を選択して下さい。\n");
         }
     }
     return 0;
