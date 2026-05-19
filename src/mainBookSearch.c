e85c9603fb5c80e70989bdedb72bbd2c528a403f
Kondo-Takayuki03-TP <282408581+Kondo-Takayuki03-TP@users.noreply.github.com>
2026-05-19 10:45:51 +0900

    19日のコミット

--- src/mainBookSearch.c
+++ src/mainBookSearch.c
@@ -3,16 +3,18 @@
 
 
 int main(void) {
+
+int BookSearch(void);   /*書籍検索関数の宣言*/{
     
     char BookTitle[][100] = {"anpanman"};   /*データベース内書籍名*/
 
     char BookSearch[100];   /*入力文字格納場所*/
 
-    int i = 1;     /*ループ制御用変数*/
+    int book = 1;     /*ループ制御用変数*/
     int choice;   /*購入選択用変数*/
     
 
-    while (i == 1){
+    while (book == 1){
         /* 書籍検索ループ */
         printf("==============================\n");
         printf("書籍検索ページ\n");
@@ -20,29 +22,35 @@ int main(void) {
         printf("終了する場合は「OSHIMAI」を入力してください。\n");
         printf("書籍のタイトルを入力してください(ローマ字・読み): ");
         scanf("%s", &BookSearch);
-        printf("入力文字: %s\n", BookSearch);
+        printf("入力文字: %s\n\n", BookSearch);
 
 
-        if (BookSearch || BookTitle) {
-            printf("書籍の候補はこちら：%s \n", BookTitle);
+        if (strcmp(BookSearch, "anpanman") == 0) {
+            printf("書籍の候補はこちら：%s \n", BookTitle[0]);
             printf("買いますか？　1:はい　2:いいえ\n");
             scanf("%d", &choice);
             if (choice == 1) {
                 printf("購入いただきありがとうございます！\n");
-                break;
+                break;/*ここのbreakを貸出機能ループに入るための式に変える*/
 
             } else if (choice == 2) {
                 printf("またのご利用をお待ちしております。\n");
+                break;
+
             } else {
-                printf("無効な選択です。さようなら。\n");
+                printf("無効な選択です。数字でご回答ください。\n");
+                continue;
+                
             }
-        } else  {
-            printf("書籍が見つかりませんでした。\n");
-        }
-        if (strcmp(BookSearch, "OSHIMAI") == 0) {
+        } else if (strcmp(BookSearch, "OSHIMAI") == 0) {
             printf("書籍検索を終了します。\n");
             break;
+        } 
+        else  {
+            printf("書籍が見つかりませんでした。\n");
+            printf("文字を正確に入力してください。\n\n\n");
         }
     }
+}
     return 0;
 }
