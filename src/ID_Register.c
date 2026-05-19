ab023ed43f6d6a3e59dec0f3945ff6908bc49269
282408034+Hiroto-Maekawa-TP@users.noreply.github.com <Hiroto-Maekawa-TP>
2026-05-19 10:43:47 +0900

    変更しました

--- src/ID_Register.c
+++ src/ID_Register.c
@@ -2,13 +2,14 @@
 
 void main(void){
 
-    int i=1;
-    int num1;
-    int num2;
-    char name[100];
-    int tel;
-    char adress[200];
-    char mail[100];
+    int i=1;// ループ制御用の変数
+    int ID_num=1;//会員番号の変数    
+    int num1;// 規約読みましたかの確認
+    int num2;// 入力した情報で間違いないかの確認
+    char name[100];//名前  
+    int tel;//電話番号
+    char adress[200];//住所
+    char mail[100];//メールアドレス
 
 
     while (i == 1){
@@ -26,21 +27,27 @@ void main(void){
                 scanf("%s", adress);
                 printf("メールアドレスを入力してください > \n");
                 scanf("%s", mail);
-
-                printf("氏名は%s\n", name);
-                printf("電話番号は%d\n", tel);
-                printf("住所は%s\n", adress);
-                printf("メールアドレスは%s\n", mail);
-                printf("でお間違いないでしょうか？(1:はい 2:訂正) > \n");
-                scanf("%d",&num2);
                 i = 3;
 
+
                 while(i == 3){
+                    printf("氏名は%s\n", name);
+                    printf("電話番号は%d\n", tel);
+                    printf("住所は%s\n", adress);
+                    printf("メールアドレスは%s\n", mail);
+                    printf("でお間違いないでしょうか？(1:はい 2:訂正) > \n");
+                    scanf("%d",&num2);
+                    i=4;
+                    
                     if(num2 == 1){
                         printf("CSVファイルに保存しています。\n");
                         printf("カードを発行しています。\n");
+                        printf("あなたの会員番号は%dです。\n", ID_num);
                         printf("カードを発行しました。ご利用ありがとうございました。\n");
-                        break;
+                        ID_num++; //会員番号を1つ増やす
+                        break;                           
+
+
                     }
                     else if(num2 == 2){
                         printf("情報を打ち直してください。\n");
